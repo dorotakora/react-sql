169cb2662ced06921a335704c47e7e3e81a855ad
dorotakora <dorota.kora@o2.pl>
2019-08-26 10:26:47 +0200

    Updates

---
+++ precache-manifest.7e46ad0357272ea3e0502af87ad2dad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1430d8b63382fe0580a7c872ab34406",
+    "url": "/react-sql/index.html"
+  },
+  {
+    "revision": "170f2fdb89b87dd400a4",
+    "url": "/react-sql/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d4a5501986efa6a92ac9",
+    "url": "/react-sql/static/js/2.af9db434.chunk.js"
+  },
+  {
+    "revision": "170f2fdb89b87dd400a4",
+    "url": "/react-sql/static/js/main.a52bf471.chunk.js"
+  },
+  {
+    "revision": "c1272d8e5bccc55851db",
+    "url": "/react-sql/static/js/runtime~main.8b5a54cf.js"
+  }
+]);
