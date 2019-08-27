bb3c54c723c7720f76319bdecf701232cdc37f57
dorotakora <dorota.kora@o2.pl>
2019-08-27 12:08:15 +0200

    change table content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends React.Component {
     products: [],
     product: {
       name: "",
-      price: ""
+      price: "",
+      producer: "",
+      created_at: ""
     }
   };
 
@@ -27,7 +29,7 @@ class App extends React.Component {
   addProduct = () => {
     const { product } = this.state;
     fetch(
-      `http://localhost:4000/products/add?name=${product.name}&price=${product.price}`
+      `http://localhost:4000/products/add?name=${product.name}&price=${product.price}&producer=${product.producer}&created_at=${product.created_at}`
     )
       .then(this.getProducts)
       .catch(err => console.error(err));
@@ -49,9 +51,13 @@ class App extends React.Component {
       .catch(err => console.error(err));
   };
 
-  renderProduct = ({ product_id, name, price }) => (
-    <div key={product_id}>
-      product name: {name} Price: {price}
+  renderProduct = ({ product_id, name, price, producer, created_at }) => (
+    <div key={product_id} className="grid">
+      <span>{product_id}</span>
+      <span>{name}</span>
+      <span>{price.toFixed(2)}</span>
+      <span>{producer}</span>
+      <span>{created_at.slice(0, 10)}</span>
       <button onClick={() => this.editProduct(product_id)}>Edit</button>
       <button onClick={() => this.deleteProduct(product_id)}>Delete</button>
     </div>
@@ -62,16 +68,56 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>React with mySQL</h1>
+        <div className="grid">
+          <span>
+            <strong>Id</strong>
+          </span>
+          <span>
+            <strong>Name</strong>
+          </span>
+          <span>
+            <strong>Price</strong>
+          </span>
+          <span>
+            <strong>Producer</strong>
+          </span>
+          <span>
+            <strong>Created at</strong>
+          </span>
+          <span>
+            <strong>Edit</strong>
+          </span>
+          <span>
+            <strong>Delete</strong>
+          </span>
+        </div>
+
         {products.map(this.renderProduct)}
         <div>
+          <label>Name</label>
           <input
             value={product.name}
             onChange={e =>
               this.setState({ product: { ...product, name: e.target.value } })
             }
           />
+          <label>Price</label>
           <input
             value={product.price}
+            onChange={e =>
+              this.setState({ product: { ...product, price: e.target.value } })
+            }
+          />
+          <label>Producer</label>
+          <input
+            value={product.producer}
+            onChange={e =>
+              this.setState({ product: { ...product, price: e.target.value } })
+            }
+          />
+          <label>Created at</label>
+          <input
+            value={product.created_at}
             onChange={e =>
               this.setState({ product: { ...product, price: e.target.value } })
             }
